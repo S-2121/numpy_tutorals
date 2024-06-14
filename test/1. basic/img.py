51daf0199ca2ee93ffc15014fa2bccdef6d7e724
S-2121 <64947607+S-2121@users.noreply.github.com>
2024-06-14 23:26:34 +0900

    add

--- test/1. basic/img.py
+++ test/1. basic/img.py
@@ -3,7 +3,14 @@ import numpy as np
 
 img = cv2.imread('Lenna.png', cv2.IMREAD_COLOR)
 
-print(img)
-# cv2.imshow('image', img)
-# cv2.waitKey(0)
-# cv2.destroyAllWindows()
+cv2.imshow('image', img)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+
+cv2.imshow('image[:100,:]', img[:100,:])
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+
+cv2.imshow('image[:,100:-100]', img[:,100:-100])
+cv2.waitKey(0)
+cv2.destroyAllWindows()
