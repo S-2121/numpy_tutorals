8631dedf4ccc01b4a5500f5694bb72335a3b836c
lightstone87 <skfmjan@gmail.com>
2024-06-15 09:57:16 +0900

    '20240608'

---
+++ 강의자료/image.py
@@ -0,0 +1,10 @@
+import matplotlib.pyplot as plt
+import matplotlib.image as img
+
+image = img.imread("C:/Users/skfmj/OneDrive/바탕 화면/Amandelbloesem_반고흐_꽃피는아몬드나무.jpg")
+plt.imshow(image)
+plt.show()
+
+#0 ~255, 300#300, dtype = uint8
+
+data_try = 
--- 실습/1. basic/test.py
+++ 실습/1. basic/test.py
@@ -1,3 +1,17 @@
 import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.image as img
 
-print(np.__version__)
+# bitmap = np.random.randint(0,256,(300,300))
+
+# plt.imshow(bitmap, cmap='gray')
+# plt.show()
+
+image = img.imread("Lenna.png")
+print(image.shape)
+image[:,:,0] = image[:,:,0] * 0
+image[:,:,1] = image[:,:,1] * 1
+image[:,:,2] = image[:,:,2] * 0
+image[:,:,3] = image[:,:,3] * .5
+plt.imshow(image)
+plt.show()
