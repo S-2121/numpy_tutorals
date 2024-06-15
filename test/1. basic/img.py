16dc1c9303914c4fb864e14aec9abcad26a69042
S-2121 <64947607+S-2121@users.noreply.github.com>
2024-06-15 09:53:08 +0900

    add

--- test/1. basic/img.py
+++ test/1. basic/img.py
@@ -7,10 +7,20 @@ cv2.imshow('image', img)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
 
-cv2.imshow('image[:100,:]', img[:100,:])
+grayscale_image = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+cv2.imshow('grayscale_image', grayscale_image)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
 
-cv2.imshow('image[:,100:-100]', img[:,100:-100])
+ret, threshold_grayscale_image = cv2.threshold(grayscale_image, 127, 255, cv2.THRESH_BINARY)
+cv2.imshow('threshold_grayscale_image', threshold_grayscale_image)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
+
+# cv2.imshow('image[:100,:]', img[:100,:])
+# cv2.waitKey(0)
+# cv2.destroyAllWindows()
+
+# cv2.imshow('image[:,100:-100]', img[:,100:-100])
+# cv2.waitKey(0)
+# cv2.destroyAllWindows()
